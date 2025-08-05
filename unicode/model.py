af9cd0850a8d6882ed1bd80e50d5d11c115bebf0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-08-04 22:13:50 -0400

    model(feat): Add patch and enable adjustable merges

--- unicode/model.py
+++ unicode/model.py
@@ -1,9 +1,12 @@
 """
 @file unicode.model.py
 @ref https://aclanthology.org/P16-1162/
+@name Algorithm 1 Learn BPE operations
 """
 
+import argparse
 import collections
+import json
 import re
 
 
@@ -11,8 +14,8 @@ def get_stats(vocab):
     pairs = collections.defaultdict(int)
     for word, freq in vocab.items():
         symbols = word.split()
-    for i in range(len(symbols) - 1):
-        pairs[symbols[i], symbols[i + 1]] += freq
+        for i in range(len(symbols) - 1):
+            pairs[symbols[i], symbols[i + 1]] += freq
     return pairs
 
 
@@ -26,16 +29,28 @@ def merge_vocab(pair, v_in):
     return v_out
 
 
-vocab = {
-    "l o w </w>": 5,
-    "l o w e r </w>": 2,
-    "n e w e s t </w>": 6,
-    "w i d e s t </w>": 3,
-}
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--merges", required=False, default=10)
+    args = parser.parse_args()
 
-num_merges = 10
-for i in range(num_merges):
-    pairs = get_stats(vocab)
-    best = max(pairs, key=pairs.get)
-    vocab = merge_vocab(best, vocab)
-print(best)
+    vocab = {
+        "l o w </w>": 5,
+        "l o w e r </w>": 2,
+        "n e w e s t </w>": 6,
+        "w i d e s t </w>": 3,
+    }
+
+    num_merges = int(args.merges)
+    for i in range(num_merges):
+        pairs = get_stats(vocab)
+        if not pairs:
+            print(f"Exhausted all potential pairs! Halted at step {i}.")
+            break
+        best = max(pairs, key=pairs.get)
+        vocab = merge_vocab(best, vocab)
+
+    print("Best:")
+    print(json.dumps(best, indent=2))
+    print("Vocab:")
+    print(json.dumps(vocab, indent=2))
