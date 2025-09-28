e3ab47c254e2902c93457747e5c2dc3e7ff91e81
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-09-27 21:11:53 -0400

    embedding(feat): Add embedding driver

---
+++ examples/model/embedding.c
@@ -0,0 +1,127 @@
+/**
+ * @file examples/model/embedding.c
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "core/path.h"
+
+#include "tokenizer/model.h"
+
+
+
+struct CLIParams {
+    const char** argv;
+    int argc;
+
+    char* model_path;
+    char* prompt;
+    bool add_bos;
+    bool add_eos;
+    bool verbose;
+};
+
+void cli_usage(const char* prog) {
+    printf("Usage: %s --model S [--verbose] [--help]\n", prog);
+    printf("  --model    -m  Path to tokenizer model file (required)\n");
+    printf("  --prompt   -p  Input text to encode and decode (required)\n");
+    printf("  --add-bos  -b  Enable bos marker\n");
+    printf("  --add-eos  -e  Enable eos marker\n");
+    printf("  --verbose  -v  Enable debug output\n");
+    printf("  --help     -h  Show this help message\n");
+}
+
+void cli_free(struct CLIParams* cli) {
+    free(cli->model_path);
+    free(cli->prompt);
+}
+
+bool cli_is_arg(const char* argv, const char* l, const char* s, int argc, int i) {
+    return (strcmp(argv, l) == 0 || strcmp(argv, s) == 0) && i + 1 < argc;
+}
+
+bool cli_is_flag(const char* argv, const char* l, const char* s) {
+    return strcmp(argv, l) == 0 || strcmp(argv, s) == 0;
+}
+
+void cli_parse(struct CLIParams* cli) {
+    // Set defaults
+    cli->model_path = NULL;
+    cli->prompt = NULL;
+    cli->add_bos = false;
+    cli->add_eos = false;
+    cli->verbose = false;
+
+    for (int i = 1; i < cli->argc; ++i) {
+        if (cli_is_arg(cli->argv[i], "--model", "-m", cli->argc, i)) {
+            cli->model_path = strdup(cli->argv[++i]);
+        } else if (cli_is_arg(cli->argv[i], "--prompt", "-p", cli->argc, i)) {
+            cli->prompt = strdup(cli->argv[++i]);
+        } else if (cli_is_flag(cli->argv[i], "--add-bos", "-b")) {
+            cli->add_bos = true;
+        } else if (cli_is_flag(cli->argv[i], "--add-bos", "-b")) {
+            cli->add_eos = true;
+        } else if (cli_is_flag(cli->argv[i], "--verbose", "-v")) {
+            cli->verbose = true;
+        } else if (cli_is_flag(cli->argv[i], "--help", "-h")) {
+            cli_usage(cli->argv[0]);
+            cli_free(cli);
+            exit(EXIT_SUCCESS);
+        } else {
+            fprintf(stderr, "Unknown or incomplete option: %s\n", cli->argv[i]);
+            cli_usage(cli->argv[0]);
+            cli_free(cli);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!cli->model_path) {
+        fprintf(stderr, "Error: --input and --output are required.\n");
+        cli_usage(cli->argv[0]);
+        cli_free(cli);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    struct CLIParams cli = {.argc = argc, .argv = argv};
+    cli_parse(&cli);
+
+    // Validate input file
+    if (!path_is_file(cli.model_path)) {
+        fprintf(stderr, "Error: Model file does not exist: %s\n", cli.model_path);
+        cli_free(&cli);
+        return EXIT_FAILURE;
+    }
+
+    // Load the tokenizer model
+    Tokenizer* t = tokenizer_load(cli.model_path);
+    if (!t) {
+        cli_free(&cli);
+        return EXIT_FAILURE;
+    }
+
+    // Text to ids
+    int id_count;
+    int* ids = tokenizer_encode(t, cli.prompt, &id_count, cli.add_bos, cli.add_eos);
+    if (!ids) {
+        fprintf(stderr, "Failed to encode text!\n");
+        return EXIT_FAILURE;
+    }
+
+    // Ids to text
+    char* text = tokenizer_decode(t, ids, id_count);
+    if (!text) {
+        fprintf(stderr, "Failed to decode ids!\n");
+    }
+    free(text);
+    free(ids);
+
+    // Clean up
+    tokenizer_free(t);
+    cli_free(&cli);
+
+    return EXIT_SUCCESS;
+}
