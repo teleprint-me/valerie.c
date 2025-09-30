a2fae1293a6bc35d6f55d04152442df859b09709
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-09-29 20:28:43 -0400

    v(feat): Add drafted model structure

--- examples/model/v.c
+++ examples/model/v.c
@@ -9,47 +9,54 @@
 #include "core/type.h"
 #include "tokenizer/model.h"
 
-struct Dim {
-    int dim;  // d_model (maybe rename for clarity?)
-    int hidden;
-    int layers;
-    int heads;
-    int kv_heads;
-    int vocab_size;
-    int seq_len;
-    int head;
-};
+typedef struct Dim {
+    int d_model;  // width of the model (hidden size)
+    int hidden;  // FFN hidden dimension (usually 4 * d_model)
+    int layers;  // number of transformer blocks (depth)
+    int heads;  // number of attention heads
+    int kv_heads;  // number of key/value heads (multiquery attention)
+    int vocab_size;  // vocabulary size
+    int seq_len;  // maximum context length
+    int head_dim;  // per-head dimension (d_model / heads)
+} Dim;
 
-struct Weights {
-    float* embeddings;
+typedef struct Block {
+    float* embeddings;  // (vocab_size, d_model)
 
-    float* Wq;
-    float* Wk;
-    float* Wv;
-    float* Wo;
+    float* Wq;  // (d_model, heads * head_dim)
+    float* Wk;  // (d_model, kv_heads * head_dim)
+    float* Wv;  // (d_model, kv_heads * head_dim)
+    float* Wo;  // (heads * head_dim, d_model)
 
-    float* W1;
-    float* W2;
-    float* W3;
+    float* W1;  // (hidden_dim, d_model)
+    float* W2;  // (d_model, hidden_dim)
+    float* W3;  // (hidden_dim, d_model)
 
-    float* rms_att;
-    float* rms_ffn;
-    float* rms_final;
-};
+    float* rms_att;  // (d_model,)
+    float* rms_ffn;  // (d_model,)
+    float* rms_final;  // (d_model,)
+} Block;
 
-struct State {
-    float* x;
+typedef struct State {
+    float* x;  // (d_model,)
 
-    float* q;
-    float* k;
-    float* v;
+    float* q;  // (d_model,)
+    float* k;  // (d_model,)
+    float* v;  // (d_model,)
 
-    float* attn;
-    float* logits;
+    float* att;  // (heads, seq_len)
+    float* logits;  // (vocab_size,)
 
-    float* k_cache;
-    float* v_cache;
-};
+    float* k_cache;  // (seq_len, d_model)
+    float* v_cache;  // (seq_len, d_model)
+} State;
+
+typedef struct Valerie {
+    Block* b;
+    State* s;
+    Tokenizer* t;
+    Dim d;
+} Valerie;
 
 int main(void) {
     return 0;
