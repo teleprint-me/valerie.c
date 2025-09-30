b179655dc736c8ce913afb3e27b0844d20957615
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-09-30 03:03:09 -0400

    v(feat): Add core linear ops

--- examples/model/v.c
+++ examples/model/v.c
@@ -52,8 +52,14 @@
  * └──────────────────────────────┘
  */
 
-#include "core/path.h"
+#include <stdlib.h>
+
+#include <assert.h>
+#include <math.h>
+
 #include "core/type.h"
+#include "core/lehmer.h"
+
 #include "tokenizer/model.h"
 
 typedef struct Dim {
@@ -125,7 +131,7 @@ typedef struct State {
     float* v_cache;  // (seq_len, d_model)
 
     Q8* qattn;
-    Q8* qffn; 
+    Q8* qffn;
 } State;
 
 typedef struct Valerie {
@@ -140,6 +146,160 @@ typedef struct Valerie {
     Dim dim;
 } Valerie;
 
+/**
+ * @section Linear ops
+ * @{
+ */
+
+void* vec_new(unsigned n, DataTypeId id) {
+    void* vec = NULL;
+
+    switch (id) {
+        case TYPE_QUANT8:
+            vec = calloc(n, sizeof(Q8));
+            break;
+        case TYPE_BFLOAT16:
+            vec = calloc(n, sizeof(uint16_t));
+            break;
+        case TYPE_FLOAT32:
+        default:
+            vec = calloc(n, sizeof(float));
+            break;
+    }
+
+    return vec;
+}
+
+void vec_xavier(float* x, unsigned n, unsigned out, unsigned in) {
+#pragma omp parallel for
+    for (unsigned i = 0; i < n; i++) {
+        // xavier(fan_in, fan_out)
+        x[i] = lehmer_xavier(in, out);  // thread is local
+    }
+}
+
+// Create a row-major matrix
+void* mat_new(unsigned out, unsigned in, DataTypeId id) {
+    unsigned dim = out * in;
+    void* mat = NULL;
+
+    switch (id) {
+        case TYPE_QUANT8:
+            mat = calloc(dim, sizeof(Q8));
+            break;
+        case TYPE_BFLOAT16:
+            mat = calloc(dim, sizeof(uint16_t));
+            break;
+        case TYPE_FLOAT32:
+        default:
+            mat = calloc(dim, sizeof(float));
+            break;
+    }
+
+    return mat;
+}
+
+void mat_xavier(float* x, unsigned n, unsigned out, unsigned in) {
+#pragma omp parallel for
+    for (unsigned i = 0; i < n; i++) {
+        // xavier(fan_in, fan_out)
+        x[i] = lehmer_xavier(in, out);  // thread is local
+    }
+}
+
+// Row-major matrix multiplication (y = Wx + b)
+// bias is omitted because it's always 0
+void mat_mul(float* y, Q8* W, Q8* x, unsigned out, unsigned in) {
+#pragma omp parallel for
+    for (unsigned i = 0; i < out; i++) {
+        float sum = 0.0f;
+        for (unsigned j = 0; j < in; j++) {
+            sum += dequantize_scalar_q8(W[i * in + j]) * dequantize_scalar_q8(x[j]);
+        }
+        y[i] = sum;
+    }
+}
+
+/** @} */
+
+/**
+ * Model pipeline
+ * @{
+ */
+
+float* embed_new(unsigned vocab_size, unsigned embed_dim) {
+    float* E = mat_new(vocab_size, embed_dim, TYPE_FLOAT32);
+    if (!E) {
+        return NULL;
+    }
+
+    mat_xavier(E, vocab_size * embed_dim, vocab_size, embed_dim);
+    return E;
+}
+
+/** @} */
+
+/**
+ * Model ops
+ * @{
+ */
+
+/// @todo Add precomputed rotary cache.
+void rotary(float* x, int pos, unsigned head_dim) {
+    unsigned half_dim = head_dim / 2;
+
+    for (unsigned i = 0; i < half_dim; i++) {
+        float angle = pos * powf(1e6f, -(float) i / half_dim);
+        float cos_a = cosf(angle), sin_a = sinf(angle);
+
+        float real = x[i];
+        float imag = x[i + half_dim];
+
+        x[i] = real * cos_a - imag * sin_a;
+        x[i + half_dim] = real * sin_a + imag * cos_a;
+    }
+}
+
+void rmsnorm(float* y, float* w, float* x, unsigned n) {
+    // Avoid division by 0
+    assert(n > 0 && "Division by zero!");
+
+    // calculate sum of squares
+    float sos = 0.0f;
+    for (unsigned i = 0; i < n; i++) {
+        sos += x[i] * x[i];
+    }
+    sos = 1.0f / sqrtf((sos / n) + 1e-6f);
+
+    // normalize and scale
+    for (unsigned i = 0; i < n; i++) {
+        y[i] = w[i] * (sos * x[i]);
+    }
+}
+
+// Applied to multi-head self-attention
+void softmax(float* x, unsigned n) {
+    float max_score = x[0];
+    for (unsigned i = 1; i < n; i++) {
+        if (x[i] > max_score) {
+            max_score = x[i];
+        }
+    }
+
+    float sum = 0.0f;
+    for (unsigned i = 0; i < n; i++) {
+        x[i] = expf(x[i] - max_score);
+        sum += x[i];
+    }
+
+    for (unsigned i = 0; i < n; i++) {
+        x[i] /= sum;
+    }
+}
+
+/** @} */
+
 int main(void) {
+    lehmer_init(1337);
     return 0;
 }
