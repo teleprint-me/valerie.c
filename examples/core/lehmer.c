fc4abebfebf2f547e4c0e654ac6c2eb4682d7801
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-10-01 04:39:42 -0400

    lehmer(patch): Include OpenMP to smoke test PRNG

--- examples/core/lehmer.c
+++ examples/core/lehmer.c
@@ -6,17 +6,35 @@
 
 #include <stdio.h>
 
+#define LIMIT 10
+
 int main(void) {
     lehmer_init(1337);
 
     printf("lehmer 32-bit int:\n");
-    for (unsigned i = 0; i < 20; i++) {
+    for (unsigned i = 0; i < LIMIT; i++) {
         printf("%d -> %d\n", i, lehmer_int32());
     }
     printf("\n");
 
     printf("lehmer 32-bit float:\n");
-    for (unsigned i = 0; i < 20; i++) {
+    for (unsigned i = 0; i < LIMIT; i++) {
+        printf("%d -> % .6f\n", i, (double) lehmer_float());
+    }
+    printf("\n");
+
+    lehmer_init(1337);
+
+    printf("lehmer 32-bit int (OpenMP):\n");
+#pragma omp parallel for
+    for (unsigned i = 0; i < LIMIT; i++) {
+        printf("%d -> %d\n", i, lehmer_int32());
+    }
+    printf("\n");
+
+    printf("lehmer 32-bit float (OpenMP):\n");
+#pragma omp parallel for
+    for (unsigned i = 0; i < LIMIT; i++) {
         printf("%d -> % .6f\n", i, (double) lehmer_float());
     }
 
