b3f6c6239680a9746a46ed239f7ebbf97afdf1fa
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-10-01 23:15:55 -0400

    v(feat): Integrate acts, mats, and blks

--- examples/model/v.c
+++ examples/model/v.c
@@ -62,8 +62,12 @@
 
 #include "tokenizer/model.h"
 
+#include "model/activation.h"
+#include "model/matrix.h"
+#include "model/blocks.h"
+
 /**
- * @section Model dimensions
+ * @section Dimensions
  */
 
 typedef struct Dim {
@@ -72,81 +76,50 @@ typedef struct Dim {
     int layers;  // number of transformer blocks (depth)
     int heads;  // number of attention heads
     int head_dim;  // per-head dimension (d_model / heads)
-    int proj_dim; // heads * head_dim
-    int kv_dim; // kv_heads * head_dim
-    int kv_mul; // multi-query attention (heads / kv_heads)
+    int proj_dim;  // heads * head_dim
+    int kv_dim;  // kv_heads * head_dim
+    int kv_mul;  // multi-query attention (heads / kv_heads)
     int kv_heads;  // number of key/value heads (multiquery attention)
     int vocab_size;  // vocabulary size
     int seq_len;  // maximum context length
 } Dim;
 
 /**
- * @section Model forward
+ * @section Forward
  */
 
 typedef struct Attention {
-    Q8* Wq;  // (d_model, n_heads * head_dim)
-    Q8* Wk;  // (d_model, n_kv_heads * head_dim)
-    Q8* Wv;  // (d_model, n_kv_heads * head_dim)
-    Q8* Wo;  // (n_heads * head_dim, d_model)
+    quant8_t* Wq;  // (d_model, n_heads * head_dim)
+    quant8_t* Wk;  // (d_model, n_kv_heads * head_dim)
+    quant8_t* Wv;  // (d_model, n_kv_heads * head_dim)
+    quant8_t* Wo;  // (n_heads * head_dim, d_model)
 
     float* rms;  // (d_model,) RMSNorm params
 } Attention;
 
 typedef struct FeedForward {
-    Q8* W1;  // (hidden, d_model)
-    Q8* W2;  // (d_model, hidden)
-    Q8* W3;  // (hidden, d_model)
+    quant8_t* W1;  // (hidden, d_model)
+    quant8_t* W2;  // (d_model, hidden)
+    quant8_t* W3;  // (hidden, d_model)
 
     float* rms;  // (d_model,) RMSNorm params
 } FeedForward;
 
-typedef struct Block {
+typedef struct Layer {
     Attention attn;  // multi-headed self attention
     FeedForward ffn;  // feed-forward network
     float* rms_attn;  // (d_model,) RMSNorm params
     float* rms_ffn;  // (d_model,) RMSNorm params
-} Block;
-
-/**
- * @section Model backward
- */
-
-typedef struct AttentionOpt {
-    // Derivatives
-    uint16_t* dWk;
-    uint16_t* dWq;
-    uint16_t* dWv;
-    uint16_t* dWo;
-
-    // Velocities
-    uint16_t* vWk;
-    uint16_t* vWq;
-    uint16_t* vWv;
-    uint16_t* vWo;
-} AttentionOpt;
-
-typedef struct FeedForwardOpt {
-    // Derivatives
-    uint16_t* dW1;
-    uint16_t* dW2;
-    uint16_t* dW3;
-
-    // Velocities
-    uint16_t* vW1;
-    uint16_t* vW2;
-    uint16_t* vW3;
-} FeedForwardOpt;
-
-typedef struct BlockOpt {
-    AttentionOpt attn;
-    FeedForwardOpt ffn;
-    float* drms_attn;  // (d_model,) RMSNorm params
-    float* drms_ffn;  // (d_model,) RMSNorm params
-} BlockOpt;
+} Layer;
 
 typedef struct State {
+    /// @note Output weights are tied to embeddings
+    /// @ref https://arxiv.org/abs/1608.05859
+    float* embeddings;  // (vocab_size, d_model)
+
     float* x;  // (d_model,)
+
+    float* rms;  // (d_model,)
 
     float* q;  // (d_model,)
     float* k;  // (d_model,)
@@ -158,97 +131,53 @@ typedef struct State {
     float* k_cache;  // (seq_len, d_model)
     float* v_cache;  // (seq_len, d_model)
 
-    Q8* qattn;
-    Q8* qffn;
+    quant8_t* qattn;
+    quant8_t* qffn;
 } State;
 
 typedef struct Valerie {
-    /// @note Output weights are tied to embeddings
-    /// @ref https://arxiv.org/abs/1608.05859
-    float* embeddings;  // (vocab_size, d_model)
-    float* rms;  // (d_model,)
-
-    Block* blocks;
-    State* state;
     Tokenizer* t;
+    Layer* layers;
+    State state;
     Dim dim;
 } Valerie;
 
 /**
- * @section Linear ops
- * @{
+ * @section Backward
  */
 
-void* vec_new(unsigned n, DataTypeId id) {
-    void* vec = NULL;
+typedef struct AttentionOpt {
+    // Derivatives
+    bfloat16_t* dWk;
+    bfloat16_t* dWq;
+    bfloat16_t* dWv;
+    bfloat16_t* dWo;
 
-    switch (id) {
-        case TYPE_QUANT8:
-            vec = calloc(n, sizeof(Q8));
-            break;
-        case TYPE_BFLOAT16:
-            vec = calloc(n, sizeof(uint16_t));
-            break;
-        case TYPE_FLOAT32:
-        default:
-            vec = calloc(n, sizeof(float));
-            break;
-    }
+    // Velocities
+    bfloat16_t* vWk;
+    bfloat16_t* vWq;
+    bfloat16_t* vWv;
+    bfloat16_t* vWo;
+} AttentionOpt;
 
-    return vec;
-}
+typedef struct FeedForwardOpt {
+    // Derivatives
+    bfloat16_t* dW1;
+    bfloat16_t* dW2;
+    bfloat16_t* dW3;
 
-void vec_xavier(float* x, unsigned n, unsigned out, unsigned in) {
-#pragma omp parallel for
-    for (unsigned i = 0; i < n; i++) {
-        // xavier(fan_in, fan_out)
-        x[i] = lehmer_xavier(in, out);  // thread is local
-    }
-}
+    // Velocities
+    bfloat16_t* vW1;
+    bfloat16_t* vW2;
+    bfloat16_t* vW3;
+} FeedForwardOpt;
 
-// Create a row-major matrix
-void* mat_new(unsigned out, unsigned in, DataTypeId id) {
-    unsigned dim = out * in;
-    void* mat = NULL;
-
-    switch (id) {
-        case TYPE_QUANT8:
-            mat = calloc(dim, sizeof(Q8));
-            break;
-        case TYPE_BFLOAT16:
-            mat = calloc(dim, sizeof(uint16_t));
-            break;
-        case TYPE_FLOAT32:
-        default:
-            mat = calloc(dim, sizeof(float));
-            break;
-    }
-
-    return mat;
-}
-
-void mat_xavier(float* x, unsigned n, unsigned out, unsigned in) {
-#pragma omp parallel for
-    for (unsigned i = 0; i < n; i++) {
-        // xavier(fan_in, fan_out)
-        x[i] = lehmer_xavier(in, out);  // thread is local
-    }
-}
-
-// Row-major matrix multiplication (y = Wx + b)
-// bias is omitted because it's always 0
-void mat_mul(float* y, Q8* W, Q8* x, unsigned out, unsigned in) {
-#pragma omp parallel for
-    for (unsigned i = 0; i < out; i++) {
-        float sum = 0.0f;
-        for (unsigned j = 0; j < in; j++) {
-            sum += dequantize_scalar_q8(W[i * in + j]) * dequantize_scalar_q8(x[j]);
-        }
-        y[i] = sum;
-    }
-}
-
-/** @} */
+typedef struct LayerOpt {
+    AttentionOpt attn;
+    FeedForwardOpt ffn;
+    float* drms_attn;  // (d_model,) RMSNorm params
+    float* drms_ffn;  // (d_model,) RMSNorm params
+} LayerOpt;
 
 /**
  * Model pipeline
@@ -263,68 +192,6 @@ float* embed_new(unsigned vocab_size, unsigned embed_dim) {
 
     mat_xavier(E, vocab_size * embed_dim, vocab_size, embed_dim);
     return E;
-}
-
-/** @} */
-
-/**
- * Model ops
- * @{
- */
-
-/// @todo Add precomputed rotary cache.
-/// Applied to feed-forward method.
-void rotary(float* x, int pos, unsigned head_dim) {
-    unsigned half_dim = head_dim / 2;
-
-    for (unsigned i = 0; i < half_dim; i++) {
-        float angle = pos * powf(1e6f, -(float) i / half_dim);
-        float cos_a = cosf(angle), sin_a = sinf(angle);
-
-        float real = x[i];
-        float imag = x[i + half_dim];
-
-        x[i] = real * cos_a - imag * sin_a;
-        x[i + half_dim] = real * sin_a + imag * cos_a;
-    }
-}
-
-// Applied to feed-forward method.
-void rmsnorm(float* y, float* w, float* x, unsigned n) {
-    // Avoid division by 0
-    assert(n > 0 && "Division by zero!");
-
-    // calculate sum of squares
-    float sos = 0.0f;
-    for (unsigned i = 0; i < n; i++) {
-        sos += x[i] * x[i];
-    }
-    sos = 1.0f / sqrtf((sos / n) + 1e-6f);
-
-    // normalize and scale
-    for (unsigned i = 0; i < n; i++) {
-        y[i] = w[i] * (sos * x[i]);
-    }
-}
-
-// Applied to multi-head self-attention
-void softmax(float* x, unsigned n) {
-    float max_score = x[0];
-    for (unsigned i = 1; i < n; i++) {
-        if (x[i] > max_score) {
-            max_score = x[i];
-        }
-    }
-
-    float sum = 0.0f;
-    for (unsigned i = 0; i < n; i++) {
-        x[i] = expf(x[i] - max_score);
-        sum += x[i];
-    }
-
-    for (unsigned i = 0; i < n; i++) {
-        x[i] /= sum;
-    }
 }
 
 /** @} */
